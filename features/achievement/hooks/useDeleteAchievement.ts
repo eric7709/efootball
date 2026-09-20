e15c836f98239a6da17dc8522131506b6c029fd3
Eric <emekaeric12@gmail.com>
2026-09-20 05:35:44 +0100

    Removed unneccessary comma

--- features/achievement/hooks/useDeleteAchievement.ts
+++ features/achievement/hooks/useDeleteAchievement.ts
@@ -3,4 +3,4 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { queryKeys } from "@/lib/api/query-keys";
 import{achievementRepository}from"./api.achievement";export function useDeleteAchievement(){const c = useQueryClient();return useMutation({
     mutationFn:(id:string)=>achievementRepository.delete(id),
-    onSuccess:({id})=>{c.removeQueries({queryKey:queryKeys.achievements.detail(id)});c.invalidateQueries({queryKey:queryKeys.achievements.all})}});}
+    onSuccess:()=>{c.removeQueries({queryKey:queryKeys.achievements.detail('1')});c.invalidateQueries({queryKey:queryKeys.achievements.all})}});}
